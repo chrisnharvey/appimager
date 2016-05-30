f493d207956970c0148de21b9e477fa4f5f7ff9e
Chris Harvey <chris@chrisnharvey.com>
2016-05-31 00:02:39 +0100

    Making data class more abstract and additing the ability to get work path and out path

--- core/data.py
+++ core/data.py
@@ -2,14 +2,21 @@ import os
 import yaml
 
 class Data:
-    def get_build_path(self):
-        cwd = os.getcwd()
-        default = cwd + "/build"
+    def get_work_path(self):
+        return self.get_path('work')
 
+    def get_out_path(self):
+        return self.get_path('out')
+
+    def get_path(self, key):
+        cwd = os.getcwd()
+        default = cwd + "/" + key
+
+        yaml_key = key + '_path'
         yaml_data = self.get_yml_data()
 
-        if 'build_path' in yaml_data:
-            return cwd + '/' + yaml_data['build_path']
+        if yaml_key in yaml_data:
+            return cwd + '/' + yaml_data[yaml_key]
 
         return default
 
