4af3c87010244407906798c3bd4783eb485131c6
Chris Harvey <chris@chrisnharvey.com>
2016-06-15 19:28:57 +0100

    Adding base build command

--- cli/build.py
+++ cli/build.py
@@ -1,4 +1,5 @@
-from cli import base
+from docker import Client
+from core import data
 from cement.core.controller import CementBaseController, expose
 
 class BuildController(CementBaseController):
@@ -8,4 +9,14 @@ class BuildController(CementBaseController):
 
     @expose(help='Run the build command/script to compile your application.')
     def build(self):
-        self.app.log.info("Build command")
+        data_obj = data.Data()
+        container_name = data_obj.get_path_hash()
+        yml_data = data_obj.get_yml_data()
+
+        docker = Client()
+
+        cmd = docker.exec_create(container_name, yml_data['build'])
+        cmd_id = cmd['Id']
+
+        for line in docker.exec_start(cmd_id, stream=True):
+            print(line.decode('ascii'), end="")
