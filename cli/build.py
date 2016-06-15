281f581b054e6563bd258acb6940bfc1f87edc99
Chris Harvey <chris@chrisnharvey.com>
2016-06-15 20:25:09 +0100

    cd into our working directory, and configure permissions

--- cli/build.py
+++ cli/build.py
@@ -1,3 +1,4 @@
+import os
 from docker import Client
 from core import data
 from cement.core.controller import CementBaseController, expose
@@ -15,8 +16,12 @@ class BuildController(CementBaseController):
 
         docker = Client()
 
-        cmd = docker.exec_create(container_name, yml_data['build'])
+        cmd = docker.exec_create(container_name, 'sh -c "cd /mnt/appimager && ' + yml_data['build'] + '"')
         cmd_id = cmd['Id']
 
         for line in docker.exec_start(cmd_id, stream=True):
             print(line.decode('ascii'), end="")
+
+        print('Configuring permissions...')
+        cmd = docker.exec_create(container_name, '/bin/sh -c "chown -R ' + str(os.getuid()) + ':' + str(os.getgid()) + ' /mnt/appimager/build"')
+        docker.exec_start(cmd['Id'])
