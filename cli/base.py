596baf834a5da58de22c6a5b124ecff06501ba0a
Chris Harvey <chris@chrisnharvey.com>
2016-06-15 19:28:37 +0100

    Show help by default

--- cli/base.py
+++ cli/base.py
@@ -4,3 +4,8 @@ class BaseController(CementBaseController):
     class Meta:
         label = 'base'
         description = "AppImager provides a build environment and tools for managing AppImage dependencies, assisting in the creation of AppDir's and creating AppImages from source code."
+
+
+    @expose(hide=True)
+    def default(self):
+        self.app.args.print_help()
