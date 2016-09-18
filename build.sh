a32c6f0ba40b49d65a8bc24169a3442e305cc18c
Chris Harvey <chris@chrisnharvey.com>
2016-09-18 19:48:40 +0100

    Use python3.5

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd /mnt/appimager/work
 wget -O /mnt/appimager/work/virtualenv.tar.gz https://pypi.python.org/packages/8b/2c/c0d3e47709d0458816167002e1aa3d64d03bdeb2a9d57c5bd18448fd24cd/virtualenv-15.0.3.tar.gz#md5=a5a061ad8a37d973d27eb197d05d99bf
 tar xf /mnt/appimager/work/virtualenv.tar.gz
 cd virtualenv*
-python3 setup.py install
+python3.5 setup.py install
 
 virtualenv /mnt/appimager/build
 
